c860e3833a5386647ca76785d1e5198103e77f3c
diego franco <diegofernandofranco@gmail.com>
2025-03-10 01:31:47 -0300

    cambios

--- functions/save-point.js
+++ functions/save-point.js
@@ -1,11 +1,8 @@
 exports.handler = async (event) => {
-  const { lat, lng, imageUrl, layer } = JSON.parse(event.body);
-
-  // Aquí simula guardar los datos con la capa; luego lo conectamos al mapa final
-  console.log(`Punto recibido: Lat ${lat}, Lng ${lng}, Foto: ${imageUrl}, Capa: ${layer}`);
-
+  const { lat, lng, imageUrl, layer, description } = JSON.parse(event.body);
+  console.log(`Punto recibido: Lat ${lat}, Lng ${lng}, Foto: ${imageUrl}, Capa: ${layer}, Descripción: ${description}`);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Punto guardado, pronto aparecerá en el mapa' })
+    body: JSON.stringify({ message: "Punto guardado, pronto aparecerá en el mapa" })
   };
 };
