de4f2d3a7967b87181bec064a98c339c3c689b23
diego franco <diegofernandofranco@gmail.com>
2025-03-15 21:11:36 -0300

    Soporte para múltiples imágenes y popup más grande

--- functions/savePoint.js
+++ functions/savePoint.js
@@ -1,16 +1,16 @@
 exports.handler = async (event) => {
-  const { latitude, longitude, imageUrl, layer, user, title, description } = JSON.parse(event.body);
-  console.log(`Punto recibido: Lat ${latitude}, Lng ${longitude}, Foto: ${imageUrl}, Capa: ${layer}, Usuario: ${user}, Título: ${title}, Descripción: ${description}`);
+  const { latitude, longitude, imageUrls, layer, user, title, description } = JSON.parse(event.body);
+  console.log(`Punto recibido: Lat ${latitude}, Lng ${longitude}, Fotos: ${imageUrls}, Capa: ${layer}, Usuario: ${user}, Título: ${title}, Descripción: ${description}`);
 
   const newFeature = {
     type: 'Feature',
     geometry: { type: 'Point', coordinates: [longitude, latitude] },
-    properties: { layer, user, title, description: imageUrl ? `${description} ${imageUrl}` : description }
+    properties: { layer, user, title, description, imageUrls }
   };
 
   const geojson = {
     type: 'FeatureCollection',
-    features: [newFeature] // Solo el nuevo punto por ahora
+    features: [newFeature]
   };
 
   return {
