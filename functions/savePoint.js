aeae6231929d5b13a6475f356f98c816ad306f60
diego franco <diegofernandofranco@gmail.com>
2025-03-16 22:11:20 -0300

    Agregar URLs e IDs reales de Gist para lanzamiento

--- functions/savePoint.js
+++ functions/savePoint.js
@@ -2,9 +2,9 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
   const { layer, point } = JSON.parse(event.body);
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN; // Configurá en Netlify
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
   const gistId = {
-    'fisuras': '4b5ae0b59d4cbbad161f0234906cb1cd',
+    'fisuras': 'ca1084559d9fc6c5a91a6175d424bd2c',
     'limpieza': '52640ef61feae0ba47fa5c1b50a4bb03',
     'trapitos': '4b5ae0b59d4cbbad161f0234906cb1cd',
     'narcomenudeo': '363f43d4842fa35a3357ae2c474a6fff',
@@ -13,17 +13,14 @@ exports.handler = async (event) => {
     'via-publica': '79af8d490561594d4bd4f739ae5e9356'
   }[layer];
 
-  // Obtener GeoJSON actual
   const gistResponse = await fetch(`https://api.github.com/gists/${gistId}`, {
     headers: { Authorization: `token ${GITHUB_TOKEN}` }
   });
   const gistData = await gistResponse.json();
   const currentGeoJSON = JSON.parse(gistData.files[`${layer}-temporal.geojson`].content || '{"type":"FeatureCollection","features":[]}');
 
-  // Agregar nuevo punto
   currentGeoJSON.features.push(point);
 
-  // Actualizar Gist
   await fetch(`https://api.github.com/gists/${gistId}`, {
     method: 'PATCH',
     headers: { Authorization: `token ${GITHUB_TOKEN}`, 'Content-Type': 'application/json' },
